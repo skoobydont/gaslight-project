8df31ae86e9ef395096476e60ffddddd72fb90b1
skoobydont <31373232+skoobydont@users.noreply.github.com>
2020-07-23 20:37:47 -0400

    more server adjustments

--- server.js
+++ server.js
@@ -25,7 +25,7 @@ app.use(express.json())
 app.use(express.static(path.join(__dirname+'client/build')));
 
 //catchall handler: serve react's index.html file
-app.get('/', (req, res) => {
+app.get('*', (req, res) => {
     res.sendFile(path.join(__dirname, '/client/build/index.html'));
 });
 
